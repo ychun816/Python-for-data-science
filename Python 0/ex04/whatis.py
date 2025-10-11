352c2d86b4959d30f693cfbca999360865b7aa75
ychun816 <ychun8lin@gmail.com>
2025-10-11 17:14:46 +0200

    ex4 fini

--- Python 0/ex04/whatis.py
+++ Python 0/ex04/whatis.py
@@ -1,4 +1,25 @@
 
+import sys
+# print(sys.argv)
+
+arg = sys.argv
+
+
+if len(arg) == 1:
+    exit()
+elif len(arg) > 2:
+    print("AssertionError: more than one argument is provided")
+else: 
+    try:
+        nb = int(arg[1])
+        if int(arg[1]) % 2 != 0:
+            print("I'm Odd.")
+        elif int(arg[1]) % 2 == 0:
+            print("I'm Even.")
+    except ValueError:
+        print("AssertionError: argument is not an integer")
+
+# return 0
 
 
 
@@ -19,3 +40,17 @@
 # $>
 # $> python whatis.py 13 5
 # AssertionError: more than one argument is provided
+
+
+
+
+#### NOTES ######################
+
+#learn to take arg 
+
+# sys.argv is a list of strings.
+# sys.argv[0] → script name (whatis.py)
+# sys.argv[1] → first argument passed
+
+
+################################
