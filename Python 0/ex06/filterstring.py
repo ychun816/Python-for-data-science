ac7765da83058bb632930080f63d2c843a6c4097
ychun816 <ychun8lin@gmail.com>
2025-10-13 15:35:50 +0200

    ex6 fini

--- Python 0/ex06/filterstring.py
+++ Python 0/ex06/filterstring.py
@@ -1,31 +1,57 @@
 #!/usr/bin/env python3
 
 import sys
+import ft_filter
 
 def main():
     """Filter words from string that are longer than N."""
 
     #check arg num
-    if len(sys.argv != 3):
+    if len(sys.argv) != 3:
         print("AssertionError: the arguments are bad")
         return
 
+    #assign args
     arg_str = sys.argv[1]
+    arg_n = sys.argv[2]
 
     try:
-        nb = int(sys.argv[1])
-    except:
+        N = int(arg_n) #cover to int
+    except ValueError:
         print("AssertionError: the arguments are bad")
         return
 
+    #Split string into words
     words_list = arg_str.split()
-    filter_words = [word for word in words_list if (lambda w: len(w) > nb)(word)] ####? 
-    print(filtered_word)
+
+    #Define a lambda for readability
+    is_longer = lambda w: len(w) > N
+
+    #Use list comprehension to filter
+    filter_words = ft_filter.ft_filter(is_longer, words_list)
+    # filter_words = [word for word in words_list if is_longer(word)]  
+    print(filter_words)
 
 
 
 if __name__ == "__main__":
     try:
         main()
-    except: Exception as e:
+    except Exception as e:
         print(f"Error: {e}")
+
+
+
+### NOTES ###
+
+# function_name(argument1, argument2)
+# -> ft_filter → the function you defined (your custom version of Python’s built-in filter())
+# -> is_longer(arg1) → another function, passed as data (not called yet!)
+# -> words_list(arg2) → the list you want to filter
+
+# def ft_filter(function, iterable):
+#     return [item for item in iterable if function(item)]
+# -> take each item in iterable
+# -> call function(item) (for example, is_longer(item))
+# -> if it returns True, keep it in the list
+#############
--- Python 0/ex06/ft_filter.py
+++ Python 0/ex06/ft_filter.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
-
 # define a function -> tores the instructions for later use
 def ft_filter(function, iterable):
     """Return a list of elements from iterable where function(element) is True."""
-    return [item for item in literable if function(item)] #=> function(item) #-> is_even(item)
+    return [item for item in iterable if function(item)] #=> function(item) #-> is_even(item)
 
     is_even = lambda x: x % 2 == 0
     numbers = [1, 2, 3, 4, 5]
 
     print(ft_filter(is_even, numbers))  # [2, 4]
 
-#the actual usage of the function
-#They call ft_filter with real values
-is_even = lambda x: x % 2 == 0
-nbs = [1, 2, 3, 4, 5]
-print(ft_filter(is_even, nbs))
+if __name__ == "__main__":
+    #the actual usage of the function
+    #They call ft_filter with real values
+    is_even = lambda x: x % 2 == 0
+    nbs = [1, 2, 3, 4, 5]
+    print(ft_filter(is_even, nbs))
 
 #OUTPUT
 # $> python filterstring.py 'Hello the World' 4
