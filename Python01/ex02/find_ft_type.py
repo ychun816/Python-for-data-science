e4d3688326a131425f35eb4d89cf6c201bbfa9df
Yi-Chun LIN <105208178+ychun816@users.noreply.github.com>
2025-10-15 16:37:12 +0000

    starting ex2

---
+++ Python01/ex02/find_ft_type.py
@@ -0,0 +1,5 @@
+#! usr/bin/env python3
+
+def ft_load(path: str) -> array: (you can return to the desired format)
+
+#your code here
---
+++ Python01/ex02/tester.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+from load_image import ft_load
+
+print(ft_load("landscape.jpg"))
+
+#OUTPUT
+# $> python tester.py
+# The shape of image is: (257, 450, 3)
+# [[[19 42 83]
+#   [23 42 84]
+# [28 43 84] ...
+# [ 0 0 0] [ 1 1 1]
+# [ 1 1 1]]] $>
