fff55655d4a45bdc4769f913194eb1889ada62f2
ychun816 <ychun8lin@gmail.com>
2025-10-27 19:34:51 +0100

    on ex02

---
+++ Python02/ex02/__main__.py
@@ -0,0 +1,24 @@
+#! usr/bin/env python3
+
+
+import sys
+import os
+
+
+def main(argv):
+    try:
+        assert len(sys.argv) == 3, "Usage: __main__.py '<test_country>' '<compare_country>'"
+        population_total(sys.argv[1], sys.argv[2])
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 1 # sys.exit(1)
+    return 0 # sys.exit(0)
+
+
+if __name__ == "__main__":
+    try:
+        main(sys.argv)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        # sys.exit(1)
---
+++ Python02/ex02/aff_pop.py
@@ -0,0 +1,18 @@
+#! usr/bin/env python3
+
+
+import sys
+import os                     # for file path handling
+import numpy as np            # for array manipulation
+import pandas as pd           # for DataFrame handling
+import seaborn as sns
+from matplotlib import pyplot as plt
+from matplotlib import ticker as plticker
+
+from load_csv import load
+
+
+
+
+def population_total(test_country:str, compare_country: str) -> None:
+    pass
