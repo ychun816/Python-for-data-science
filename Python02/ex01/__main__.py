0814100dc626c6b8766cd2138a58725b8786f5e3
Yi-Chun LIN <105208178+ychun816@users.noreply.github.com>
2025-11-06 00:28:43 +0000

    norm fixed ex01

--- Python02/ex01/__main__.py
+++ Python02/ex01/__main__.py
@@ -1,30 +1,23 @@
-#! usr/bin/env python3
+#!/usr/bin/env python3
 
 import sys
-import os                     # for file path handling
-import numpy as np            # for array manipulation
-import pandas as pd           # for DataFrame handling
-import seaborn as sns         # for plotting
-
-
-from matplotlib import pyplot as plt
-from matplotlib import ticker as plticker
 
 from aff_life import life_expectancy
-from load_csv import load
+
 
 def main(argv):
+    if len(argv) != 2:
+        print("Usage: __main__.py '<country_name>'")
+        return 1
+
     try:
-        assert len(sys.argv) == 2, "Usage: __main__.py '<country_name>'"
-        life_expectancy(sys.argv[1])
+        life_expectancy(argv[1])
     except Exception as e:
         print(f"An error occurred: {e}")
-        return 1 # sys.exit(1)
-    return 0 # sys.exit(0)
+        return 1
+
+    return 0
+
 
 if __name__ == "__main__":
-    try:
-        main(sys.argv)
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        # sys.exit(1)
+    sys.exit(main(sys.argv))
