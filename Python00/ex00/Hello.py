e397c4ccd914fd3746a754e20f690480e3b09d7c
ychun816 <ychun8lin@gmail.com>
2025-11-07 16:48:42 +0100

    add env check in notes

--- Python00/ex00/Hello.py
+++ Python00/ex00/Hello.py
@@ -27,3 +27,52 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+
+
+### PRE-CHECK ###
+
+## VERIFY
+## verify if python installed
+# python --version
+# python3 --version
+# python2 --version
+
+## verify flake8(norminette)
+# norminette --version
+# flake8 --version (if alias exists)
+
+
+## if python not installed -> install python:
+
+# download and install Miniconda into your home
+# curl -L -o ~/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# bash ~/miniconda.sh -b -p $HOME/miniconda3
+
+# # initialize for zsh and reload shell
+# $HOME/miniconda3/bin/conda init zsh
+# source ~/.zshrc
+
+# # create a Python 3.10 environment and activate
+# conda create -n py310 python=3.10 -y
+# conda activate py310
+
+# # verify
+# python --version
+# pip install --upgrade pip
+# pip install flake8
+
+
+## if no flake8(norminette) -> install flake8(norminette):
+# python --version
+# pip install --upgrade pip
+# pip install flake8
+
+## verify python and install flake8
+# python --version
+# pip install --upgrade pip
+# pip install flake8
+
+
+
+#################
